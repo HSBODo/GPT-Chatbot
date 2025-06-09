d5303766ad9dd8f6314ac9e907aeb5575e8b1da8
한수빈 <vinsulill@gmail.com>
2025-06-09 16:54:58 +0900

    init

--- src/main/java/com/example/chatbot/common/service/impl/OpenAiServiceImpl.java
+++ src/main/java/com/example/chatbot/common/service/impl/OpenAiServiceImpl.java
@@ -21,7 +21,9 @@ public class OpenAiServiceImpl implements OpenAiService {
     private RestTemplate restTemplate = new RestTemplate();
     private ObjectMapper mapper = new ObjectMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    private String OPENAI_API_KEY ="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";
+
+    @Value("${openai.key}")
+    private String OPENAI_API_KEY;
 //    private final String ASSISTANT_ID = "asst_s9UCWodjOKCDMnkydM3Brd0T"; // 비앤빛
     private final String ASSISTANT_ID = "asst_u90wooH9QQcm59K64Q0qILBH";
 
